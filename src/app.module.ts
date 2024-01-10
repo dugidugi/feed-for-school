4c853d6e387ae4814c586591bc0b08908e7dee08
dugidugi <yooduck.h@gmail.com>
2024-01-10 20:30:58 +0900

    feat: mongo, mongoose 설정

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/user.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+      validationSchema: Joi.object({
+        MONGO_USER: Joi.string().required(),
+        MONGO_PASSWORD: Joi.string().required(),
+        MONGO_HOST: Joi.string().required(),
+      }),
+    }),
+
+    MongooseModule.forRoot(
+      `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}`,
+    ),
+    UsersModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
