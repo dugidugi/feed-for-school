80a53b72bc84173c83a75370809b238ddce66f36
dugidugi <yooduck.h@gmail.com>
2024-01-11 11:48:55 +0900

    feat: userFollow Schema 생성

---
+++ src/users/schemas/user-follow.schema.ts
@@ -0,0 +1,17 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import mongoose, { HydratedDocument } from 'mongoose';
+import { School } from 'src/schools/schemas/school.schema';
+import { User } from './user.schema';
+
+export type UserFollowDocument = HydratedDocument<UserFollow>;
+
+@Schema({ timestamps: true, id: false })
+export class UserFollow {
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'School', index: true })
+  school: School;
+
+  @Prop({ type: mongoose.Schema.Types.ObjectId, ref: 'User', index: true })
+  user: User;
+}
+
+export const UserFollowSchema = SchemaFactory.createForClass(UserFollow);
--- src/users/user.module.ts
+++ src/users/user.module.ts
@@ -3,10 +3,14 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { User, UserSchema } from './schemas/user.schema';
+import { UserFollow, UserFollowSchema } from './schemas/user-follow.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: UserFollow.name, schema: UserFollowSchema },
+    ]),
   ],
   providers: [UsersService],
   controllers: [UsersController],
